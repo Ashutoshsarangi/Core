0c23b34e1d738123088aa50a3a885b5701bf2b5e
Ashu <ashutoshsarangi95@gmail.com>
2020-08-24 19:39:28 +0530

    Added Web Form

--- microApps/WebForms+Validator/src/main.js
+++ microApps/WebForms+Validator/src/main.js
@@ -1,4 +1,85 @@
+(function () {
+  let date = new Date();
+  let dd = date.getDate();
+  let mm = date.getMonth() + 1;
+  let yyyy = date.getFullYear();
+  dd = dd < 10 ? '0' + dd : dd;
+  mm = mm < 10 ? '0' + mm : mm;
+  let today = `${yyyy}-${mm}-${dd}`;
+  document.getElementById("userDOB").setAttribute("max", today);
+})()
 function formValidator(event) {
   console.log(event);
-  test();
+  let name = event.target.dataset.bind;
+  let flag = false;
+  switch (name) {
+    case 'name':
+      // call a function from Validator plugin
+      flag = nameValidator(event.target.value);
+      if (flag) {
+        successHandler(event);
+      } else {
+        let msg = "Enter a valid Name";
+        errorHandler(event, msg);
+      }
+      break;
+    case 'email':
+      // call a function from Validator plugin
+      flag = emailValidator(event.target.value);
+      if (flag) {
+        successHandler(event);
+      } else {
+        let msg = "Enter a valid Email";
+        errorHandler(event, msg);
+      }
+      break;
+    case 'date':
+      // call a function from Validator plugin
+      let minAge = 18;
+      flag = ageRestriction(event.target.value, minAge);
+      if (flag) {
+        successHandler(event);
+      } else {
+        let msg = "Your Age Should Be more than " + minAge;
+        errorHandler(event, msg);
+      }
+      break;
+  }
+}
+function successHandler(event) {
+  resetClassHandler(event);
+  const inputId = event.target.id;
+  const iconId = event.target.dataset.iconid;
+  document.getElementById(inputId).classList.add("successBorder");
+  document.getElementById(iconId).classList.add("fa-check");
+  document.getElementById(iconId).classList.add("successIcon");
+
+}
+function errorHandler(event, msg) {
+  resetClassHandler(event);
+  const inputId = event.target.id;
+  const iconId = event.target.dataset.iconid;
+  const errorId = event.target.dataset.errorid;
+
+  document.getElementById(inputId).classList.add("errorBorder");
+  document.getElementById(iconId).classList.add("fa-exclamation");
+  document.getElementById(iconId).classList.add("errorIcon");
+  document.getElementById(errorId).classList.add("errorMessage");
+  document.getElementById(errorId).textContent = msg;
+}
+
+function resetClassHandler(event) {
+  const inputId = event.target.id;
+  const iconId = event.target.dataset.iconid;
+  const errorId = event.target.dataset.errorid;
+
+  document.getElementById(inputId).classList.remove("successBorder");
+  document.getElementById(inputId).classList.remove("errorBorder");
+
+  document.getElementById(iconId).classList.remove("fa-check");
+  document.getElementById(iconId).classList.remove("successIcon");
+  document.getElementById(iconId).classList.remove("fa-exclamation");
+  document.getElementById(iconId).classList.remove("errorIcon");
+  document.getElementById(errorId).classList.remove("errorMessage");
+  document.getElementById(errorId).textContent = "";
 }
--- plugins/validator.js
+++ plugins/validator.js
@@ -6,6 +6,25 @@ let numberValidator = (num) => {
   });
   return true;
 }
-let test = () => {
-  console.log('Executed');
+let nameValidator = (name) => {
+  let regExpression = /^[a-zA-Z ]{2,30}$/;
+  let t = regExpression.test(name);
+  console.log(t);
+  return t;
+}
+
+let emailValidator = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+}
+let ageRestriction = (date, minAge) => {
+  console.log(new Date());
+  console.log(new Date(date));
+  let ageInMs = new Date() - new Date(date);
+  let age = (ageInMs / (1000 * 3600 * 24 * 30 * 12)).toFixed();
+  if (age >= minAge) {
+    return true;
+  } else {
+    return false;
+  }
 }
