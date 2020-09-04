5d5a6798afed6113cd34ceaa40355e6eee264aac
Ashu <ashutoshsarangi95@gmail.com>
2020-09-04 10:48:16 +0530

    Calculator Code Updated.

--- microApps/Calculator/main.js
+++ microApps/Calculator/main.js
@@ -1,22 +1,23 @@
 function clickHandler(event) {
-    let result = '';
-    let btnPressed = event.target.id;
-    let text = document.getElementById("calculator").value;
-    switch (btnPressed) {
-        case "=":
-            result = calculate(text);
-            break;
-    }
-    if (btnPressed === "R") {
-        document.getElementById("calculator").value = "";
-    } else if (btnPressed === "B") {
-        // Add Logic For Back Space;
-        document.getElementById("calculator").value = text.slice(0, -1);
-    } else {
-        document.getElementById("calculator").value = text + btnPressed + result;
-    }
+  let result = '';
+  let btnPressed = event.target.id;
+  let text = document.getElementById("calculator").value;
+  switch (btnPressed) {
+    case "=":
+      result = calculate(text);
+      break;
+  }
+  if (btnPressed === "R") {
+    document.getElementById("calculator").value = "";
+  } else if (btnPressed === "B") {
+    // Add Logic For Back Space;
+    document.getElementById("calculator").value = text.slice(0, -1);
+  } else {
+    document.getElementById("calculator").value = text + btnPressed + result;
+  }
 }
 
 function calculate(text) {
-    return "145";
+  //use eval it will Calculate the Expression and Return the result
+  return eval(text);
 }
