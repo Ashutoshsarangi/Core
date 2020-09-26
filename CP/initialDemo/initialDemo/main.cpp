3f5d16fbc008940d0fb22e3592decf5590138f4c
Ashu <ashutoshsarangi95@gmail.com>
2020-09-26 10:00:40 +0530

    Added a XCode problem solving Folder.

--- CP/initialDemo/initialDemo/main.cpp
+++ CP/initialDemo/initialDemo/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0;i< n;i++){
+        cout<<i<<endl;
+    }
 }
